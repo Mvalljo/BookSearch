333f4c61350d1110bd52e3d35b5b31b225941bf9
Maria Vallejo <maria.vallejo29@yahoo.com>
2021-12-29 04:00:08 -0600

    fix resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,15 +6,19 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id });
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+
+        return userData;
       }
       throw new AuthenticationError("You need to be logged in!");
     }
   },
 
   Mutation: {
-    addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
       const token = signToken(user);
       return { token, user };
     },
@@ -35,14 +39,14 @@ const resolvers = {
 
       return { token, user };
     },
-    saveBook: async (parent, args, context) => {
+    saveBook: async (parent, { input }, context) => {
       if (context.user) {
         const updatedUser = await User.findOneAndUpdate(
-          { _id: user._id },
-          { $addToSet: { savedBooks: body } },
-          { new: true, runValidators: true }
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: input } },
+          { new: true}
         );
-        return res.json(updatedUser);
+        return updatedUser;
       }
       throw new AuthenticationError("You need to be logged in!");
     },
